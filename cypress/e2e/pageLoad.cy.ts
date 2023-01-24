66b52003651be3ed176be5e6186cb287404e9c7a
Pavol Hejny <me@pavolhejny.com>
2023-01-24 20:48:35 +0100

    Fix tests

--- cypress/e2e/pageLoad.cy.ts
+++ cypress/e2e/pageLoad.cy.ts
@@ -2,12 +2,13 @@
 
 context('Local Collboard', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:4455/');
+        cy.visit('http://localhost:5000/');
     });
 
     describe('Visiting the page', () => {
         it('should be able to see the heading', () => {
-            cy.contains(/From 0 to 1/i);
+            // cy.contains(/iport/i);
+            cy.contains(/Seznam procesů/i);
         });
     });
 });
