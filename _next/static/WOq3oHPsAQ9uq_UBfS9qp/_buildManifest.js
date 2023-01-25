6fc629ea35bfb5995fa4cfe68d7b00f8e36d00d8
Pavol Hejny <me@pavolhejny.com>
2023-01-25 21:33:43 +0100

    Updates

---
+++ _next/static/WOq3oHPsAQ9uq_UBfS9qp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/6bc40342d446a2c6.css","static/chunks/pages/index-41ae684a3691bde5.js"],"/_error":["static/chunks/pages/_error-91a854d9c9cfa29b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
