38182305d933296b55847838e47188c2b6c08201
Pavol Hejny <me@pavolhejny.com>
2023-03-09 15:07:24 +0100

    Documentation

--- src/model/interfaces/10-ServerConnector.ts
+++ src/model/interfaces/10-ServerConnector.ts
@@ -5,12 +5,18 @@ import { Process } from './20-Process';
  * Object that represents connection to server which controlls the app
  */
 export interface ServerConnector {
-    // TODO: !!! newProcessOptions;
     /**
      * Available running processes
      * Every update(next) of the observable will fully update the process list.
      */
     processList: Observable<Array<Process>>;
 
+    /**
+     * Get any process by its ID
+     */
     getProcessById(processId: string | number): Process;
 }
+
+/**
+ * TODO: !!! newProcessOptions
+ */
