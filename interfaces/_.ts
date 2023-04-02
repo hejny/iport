f48fb84d5a1bf092ce39bb279e40243b5c0e3b88
Pavol Hejny <me@pavolhejny.com>
2023-04-02 14:47:41 +0200

    Interfaces for server

--- interfaces/_.ts
+++ interfaces/_.ts
@@ -1,10 +1,14 @@
+import { IServerProcess } from '@/model/interfaces/IServerProcess';
 import { IProcessId } from '@/model/interfaces/common';
 
-export interface Response_newProcess {
+export interface Socket_Response_newProcess {
     processId: IProcessId;
 }
 
+export type Socket_Event_processes = Array<Pick<IServerProcess, 'processId' | 'processTitle' | 'menuItem'>>;
+
 /**
+ * TODO: !!! Annotate
  * TODO: !!! Make every type
  * TODO: !!! Break apart
  */
