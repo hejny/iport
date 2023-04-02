306e2b20d36698d1bfe5334fbd1d0f05d19cb9a2
Pavol Hejny <me@pavolhejny.com>
2023-04-02 21:51:05 +0200

    Add annotation marks

--- interfaces/socket.ts
+++ interfaces/socket.ts
@@ -1,40 +1,73 @@
 import { IServerProcess } from '@/model/interfaces/IServerProcess';
 import { IProcessId, IServerHtml, IServerHtmlWithInput } from '@/model/interfaces/common';
 
+/**
+ * @@@
+ */
 export type Socket_Request_startNewProcess = unknown;
 
+/**
+ * @@@
+ */
 export interface Socket_Response_newProcess {
     processId: IProcessId;
 }
+/**
+ * @@@
+ */
 export interface Socket_Error_newProcess {
     errorMessage: string;
 }
 
+/**
+ * @@@
+ */
 export type Socket_Event_processes = Array<Pick<IServerProcess, 'processId' | 'processTitle' | 'menuItem'>>;
+/**
+ * @@@
+ */
 export type Socket_Event_newProcessOptionsForm = IServerHtmlWithInput;
 
+/**
+ * @@@
+ */
 export interface Socket_Request_getProcessById {
     processId: IProcessId;
 }
 
+/**
+ * @@@
+ */
 export interface Socket_Response_getProcessById {
     processTitle: string;
     menuItem: IServerHtml;
 }
 
+/**
+ * @@@
+ */
 export interface Socket_Request_recieveInput {
     processId: IProcessId;
     input: unknown;
 }
 
+/**
+ * @@@
+ */
 export interface Socket_Subscribe_LogsAndInputFrom {
     processId: IProcessId;
 }
 
+/**
+ * @@@
+ */
 export interface Socket_Event_newLogs {
     logs: Array<IServerHtml>;
 }
 
+/**
+ * @@@
+ */
 export interface Socket_Event_inputForm {
     inputForm: IServerHtmlWithInput;
 }
