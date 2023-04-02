adcdef789994553e73790737b1bcd0649c78c729
Pavol Hejny <me@pavolhejny.com>
2023-04-02 21:55:04 +0200

    💭 Write annotations
    @generator ChatBing from Sun Apr 02 2023
    
    🔼 This commit was automatically generated by [Batch project editor](https://github.com/hejny/batch-project-editor) using workflow onceWriteAnnotations

--- interfaces/socket.ts
+++ interfaces/socket.ts
@@ -2,41 +2,41 @@ import { IServerProcess } from '@/model/interfaces/IServerProcess';
 import { IProcessId, IServerHtml, IServerHtmlWithInput } from '@/model/interfaces/common';
 
 /**
- * @@@
+ * Type for Socket_Request_startNewProcess ⁘
  */
 export type Socket_Request_startNewProcess = unknown;
 
 /**
- * @@@
+ * Interface for Socket_Response_newProcess ⁘
  */
 export interface Socket_Response_newProcess {
     processId: IProcessId;
 }
 /**
- * @@@
+ * Interface for Socket_Error_newProcess ⁘
  */
 export interface Socket_Error_newProcess {
     errorMessage: string;
 }
 
 /**
- * @@@
+ * Type for Socket_Event_processes ⁘
  */
 export type Socket_Event_processes = Array<Pick<IServerProcess, 'processId' | 'processTitle' | 'menuItem'>>;
 /**
- * @@@
+ * Type for Socket_Event_newProcessOptionsForm ⁘
  */
 export type Socket_Event_newProcessOptionsForm = IServerHtmlWithInput;
 
 /**
- * @@@
+ * Interface for Socket_Request_getProcessById ⁘
  */
 export interface Socket_Request_getProcessById {
     processId: IProcessId;
 }
 
 /**
- * @@@
+ * Interface for Socket_Response_getProcessById ⁘
  */
 export interface Socket_Response_getProcessById {
     processTitle: string;
@@ -44,7 +44,7 @@ export interface Socket_Response_getProcessById {
 }
 
 /**
- * @@@
+ * Interface for Socket_Request_recieveInput ⁘
  */
 export interface Socket_Request_recieveInput {
     processId: IProcessId;
@@ -52,21 +52,21 @@ export interface Socket_Request_recieveInput {
 }
 
 /**
- * @@@
+ * Interface for Socket_Subscribe_LogsAndInputFrom ⁘
  */
 export interface Socket_Subscribe_LogsAndInputFrom {
     processId: IProcessId;
 }
 
 /**
- * @@@
+ * Interface for Socket_Event_newLogs ⁘
  */
 export interface Socket_Event_newLogs {
     logs: Array<IServerHtml>;
 }
 
 /**
- * @@@
+ * Interface for Socket_Event_inputForm ⁘
  */
 export interface Socket_Event_inputForm {
     inputForm: IServerHtmlWithInput;
